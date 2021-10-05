e664fe72bf5061415ace5b7c4607dda19b277912
André Tashiro <andre_seichi@hotmail.com>
2021-10-05 15:04:14 -0300

    pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import avatarSvg from '../../public/images/avatar.svg';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖖 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src={avatarSvg} alt="Girl coding" />
+      </main>
     </>
   );
 }
