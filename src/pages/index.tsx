65dc42003de2d08c18a631a67249af73f2cf84a1
André Tashiro <andre_seichi@hotmail.com>
2021-10-05 17:09:25 -0300

    chamada api stripe pelo lado do next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
 import Image from 'next/image';
 
 import avatarSvg from '../../public/images/avatar.svg';
 import { SubscribeButton } from '../components/SubscribeButton';
 
+import { stripe } from '../services/stripe';
+
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -21,10 +32,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image src={avatarSvg} alt="Girl coding" />
@@ -32,3 +43,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JhIo9DQIkWjilDR19OnBdTF');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
