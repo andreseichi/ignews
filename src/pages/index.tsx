29193d2ea15d2f29dcf0cee7db0898c64db2ba89
André Tashiro <andre_seichi@hotmail.com>
2021-10-06 10:57:29 -0300

    getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 import Image from 'next/image';
@@ -44,7 +44,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JhIo9DQIkWjilDR19OnBdTF');
 
   const product = {
@@ -59,5 +59,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
