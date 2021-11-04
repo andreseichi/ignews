a7ee71f3703b9185e46d4dbf94e01aa5ce350fd5
André Tashiro <andre_seichi@hotmail.com>
2021-11-04 18:31:29 -0300

    atualizando dados no faunadb para ter o id do stripeCustomer

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client';
+import { query as q } from 'faunadb';
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
+
+type User = {
+  ref: {
+    id: string;
+  };
+};
 
 export default async function subscribe(
   req: NextApiRequest,
@@ -9,9 +17,21 @@ export default async function subscribe(
   if (req.method === 'POST') {
     const session = await getSession({ req });
 
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
+    );
+
     const stripeCustomer = await stripe.customers.create({
       email: session.user.email,
     });
+
+    await fauna.query(
+      q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+        data: {
+          stripe_customer_id: stripeCustomer.id,
+        },
+      })
+    );
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
